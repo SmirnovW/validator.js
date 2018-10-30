450803f36a28461d65983ee0be16940f9c54aa93
Vladislav Smirnov <mr.bikus@yandex.ru>
2018-10-31 00:13:53 +0400

    Changed export for supporting the three shaking

--- src/index.js
+++ src/index.js
@@ -1,100 +1,103 @@
-import toDate from './lib/toDate';
-import toFloat from './lib/toFloat';
-import toInt from './lib/toInt';
-import toBoolean from './lib/toBoolean';
-import equals from './lib/equals';
-import contains from './lib/contains';
-import matches from './lib/matches';
+export { default as toDate } from './lib/toDate';
+export { default as toFloat } from './lib/toFloat';
+export { default as toInt } from './lib/toInt';
+export { default as toBoolean } from './lib/toBoolean';
+export { default as equals } from './lib/equals';
+export { default as contains } from './lib/contains';
+export { default as matches } from './lib/matches';
 
-import isEmail from './lib/isEmail';
-import isURL from './lib/isURL';
-import isMACAddress from './lib/isMACAddress';
-import isIP from './lib/isIP';
-import isIPRange from './lib/isIPRange';
-import isFQDN from './lib/isFQDN';
+export { default as isEmail } from './lib/isEmail';
+export { default as isURL } from './lib/isURL';
+export { default as isMACAddress } from './lib/isMACAddress';
+export { default as isIP } from './lib/isIP';
+export { default as isIPRange } from './lib/isIPRange';
+export { default as isFQDN } from './lib/isFQDN';
 
-import isBoolean from './lib/isBoolean';
+export { default as isBoolean } from './lib/isBoolean';
 
 import isAlpha, { locales as isAlphaLocales } from './lib/isAlpha';
 import isAlphanumeric, { locales as isAlphanumericLocales } from './lib/isAlphanumeric';
-import isNumeric from './lib/isNumeric';
-import isPort from './lib/isPort';
-import isLowercase from './lib/isLowercase';
-import isUppercase from './lib/isUppercase';
+export { default as isNumeric } from './lib/isNumeric';
+export { default as isPort } from './lib/isPort';
+export { default as isLowercase } from './lib/isLowercase';
+export { default as isUppercase } from './lib/isUppercase';
 
-import isAscii from './lib/isAscii';
-import isFullWidth from './lib/isFullWidth';
-import isHalfWidth from './lib/isHalfWidth';
-import isVariableWidth from './lib/isVariableWidth';
-import isMultibyte from './lib/isMultibyte';
-import isSurrogatePair from './lib/isSurrogatePair';
+export { default as isAscii } from './lib/isAscii';
+export { default as isFullWidth } from './lib/isFullWidth';
+export { default as isHalfWidth } from './lib/isHalfWidth';
+export { default as isVariableWidth } from './lib/isVariableWidth';
+export { default as isMultibyte } from './lib/isMultibyte';
+export { default as isSurrogatePair } from './lib/isSurrogatePair';
 
-import isInt from './lib/isInt';
-import isFloat, { locales as isFloatLocales } from './lib/isFloat';
-import isDecimal from './lib/isDecimal';
-import isHexadecimal from './lib/isHexadecimal';
-import isDivisibleBy from './lib/isDivisibleBy';
+export { default as isInt } from './lib/isInt';
+export { default as isFloat} from './lib/isFloat';
+export { locales as isFloatLocales } from './lib/isFloat';
+export { default as isDecimal } from './lib/isDecimal';
+export { default as isHexadecimal } from './lib/isHexadecimal';
+export { default as isDivisibleBy } from './lib/isDivisibleBy';
 
-import isHexColor from './lib/isHexColor';
+export { default as isHexColor } from './lib/isHexColor';
 
-import isISRC from './lib/isISRC';
+export { default as isISRC } from './lib/isISRC';
 
-import isMD5 from './lib/isMD5';
-import isHash from './lib/isHash';
-import isJWT from './lib/isJWT';
+export { default as isMD5 } from './lib/isMD5';
+export { default as isHash } from './lib/isHash';
+export { default as isJWT } from './lib/isJWT';
 
-import isJSON from './lib/isJSON';
-import isEmpty from './lib/isEmpty';
+export { default as isJSON } from './lib/isJSON';
+export { default as isEmpty } from './lib/isEmpty';
 
-import isLength from './lib/isLength';
-import isByteLength from './lib/isByteLength';
+export { default as isLength } from './lib/isLength';
+export { default as isByteLength } from './lib/isByteLength';
 
-import isUUID from './lib/isUUID';
-import isMongoId from './lib/isMongoId';
+export { default as isUUID } from './lib/isUUID';
+export { default as isMongoId } from './lib/isMongoId';
 
-import isAfter from './lib/isAfter';
-import isBefore from './lib/isBefore';
+export { default as isAfter } from './lib/isAfter';
+export { default as isBefore } from './lib/isBefore';
 
-import isIn from './lib/isIn';
+export { default as isIn } from './lib/isIn';
 
-import isCreditCard from './lib/isCreditCard';
-import isIdentityCard from './lib/isIdentityCard';
+export { default as isCreditCard } from './lib/isCreditCard';
+export { default as isIdentityCard } from './lib/isIdentityCard';
 
-import isISIN from './lib/isISIN';
-import isISBN from './lib/isISBN';
-import isISSN from './lib/isISSN';
+export { default as isISIN } from './lib/isISIN';
+export { default as isISBN } from './lib/isISBN';
+export { default as isISSN } from './lib/isISSN';
 
-import isMobilePhone, { locales as isMobilePhoneLocales } from './lib/isMobilePhone';
+export { default as isMobilePhone} from './lib/isMobilePhone';
+export { locales as isMobilePhoneLocales } from './lib/isMobilePhone';
 
-import isCurrency from './lib/isCurrency';
+export { default as isCurrency } from './lib/isCurrency';
 
-import isISO8601 from './lib/isISO8601';
-import isRFC3339 from './lib/isRFC3339';
-import isISO31661Alpha2 from './lib/isISO31661Alpha2';
-import isISO31661Alpha3 from './lib/isISO31661Alpha3';
+export { default as isISO8601 } from './lib/isISO8601';
+export { default as isRFC3339 } from './lib/isRFC3339';
+export { default as isISO31661Alpha2 } from './lib/isISO31661Alpha2';
+export { default as isISO31661Alpha3 } from './lib/isISO31661Alpha3';
 
-import isBase64 from './lib/isBase64';
-import isDataURI from './lib/isDataURI';
-import isMagnetURI from './lib/isMagnetURI';
+export { default as isBase64 } from './lib/isBase64';
+export { default as isDataURI } from './lib/isDataURI';
+export { default as isMagnetURI } from './lib/isMagnetURI';
 
-import isMimeType from './lib/isMimeType';
+export { default as isMimeType } from './lib/isMimeType';
 
-import isLatLong from './lib/isLatLong';
-import isPostalCode, { locales as isPostalCodeLocales } from './lib/isPostalCode';
+export { default as isLatLong } from './lib/isLatLong';
+export { default as isPostalCode} from './lib/isPostalCode';
+export { locales as isPostalCodeLocales } from './lib/isPostalCode';
 
-import ltrim from './lib/ltrim';
-import rtrim from './lib/rtrim';
-import trim from './lib/trim';
-import escape from './lib/escape';
-import unescape from './lib/unescape';
-import stripLow from './lib/stripLow';
-import whitelist from './lib/whitelist';
-import blacklist from './lib/blacklist';
-import isWhitelisted from './lib/isWhitelisted';
+export { default as ltrim } from './lib/ltrim';
+export { default as rtrim } from './lib/rtrim';
+export { default as trim } from './lib/trim';
+export { default as escape } from './lib/escape';
+export { default as unescape } from './lib/unescape';
+export { default as stripLow } from './lib/stripLow';
+export { default as whitelist } from './lib/whitelist';
+export { default as blacklist } from './lib/blacklist';
+export { default as isWhitelisted } from './lib/isWhitelisted';
 
-import normalizeEmail from './lib/normalizeEmail';
+export { default as normalizeEmail } from './lib/normalizeEmail';
 
-import toString from './lib/util/toString';
+export { default as toString } from './lib/util/toString';
 
 const version = '10.8.0';
 
